207b3c1b7ea2451b6a0aabba351002cb857998ea
Brooke Bryan <brooke@bajb.net>
2023-02-21 10:30:13 +0000

    Update Log & Child setters

--- keystone/child.go
+++ keystone/child.go
@@ -1,10 +1,20 @@
 package keystone
 
-import "github.com/kubex/definitions-go/app"
+import (
+	"github.com/kubex/definitions-go/app"
+)
 
 type Child struct {
 	Type    app.ScopedKey
 	ID      string
 	Data    []byte
 	written bool
+}
+
+func (e *Entity) AddChild(childType string, data []byte) {
+	e.Children = append(e.Children, Child{
+		written: false,
+		Type:    app.NewScopedKey(childType, defaultSetGlobalAppID),
+		Data:    data,
+	})
 }
--- keystone/logs.go
+++ keystone/logs.go
@@ -14,35 +14,35 @@ type LogEntry struct {
 	Actor     *Actor
 }
 
-func (e *Entity) Debug(message, reference, traceID string) {
+func (e *Entity) LogDebug(message, reference, traceID string) {
 	e.Log(LogLevelDebug, message, reference, traceID, time.Now())
 }
 
-func (e *Entity) Info(message, reference, traceID string) {
+func (e *Entity) LogInfo(message, reference, traceID string) {
 	e.Log(LogLevelInfo, message, reference, traceID, time.Now())
 }
 
-func (e *Entity) Notice(message, reference, traceID string) {
+func (e *Entity) LogNotice(message, reference, traceID string) {
 	e.Log(LogLevelNotice, message, reference, traceID, time.Now())
 }
 
-func (e *Entity) Warn(message, reference, traceID string) {
+func (e *Entity) LogWarn(message, reference, traceID string) {
 	e.Log(LogLevelWarn, message, reference, traceID, time.Now())
 }
 
-func (e *Entity) Error(message, reference, traceID string) {
+func (e *Entity) LogError(message, reference, traceID string) {
 	e.Log(LogLevelError, message, reference, traceID, time.Now())
 }
 
-func (e *Entity) Critical(message, reference, traceID string) {
+func (e *Entity) LogCritical(message, reference, traceID string) {
 	e.Log(LogLevelCritical, message, reference, traceID, time.Now())
 }
 
-func (e *Entity) Alert(message, reference, traceID string) {
+func (e *Entity) LogAlert(message, reference, traceID string) {
 	e.Log(LogLevelAlert, message, reference, traceID, time.Now())
 }
 
-func (e *Entity) Fatal(message, reference, traceID string) {
+func (e *Entity) LogFatal(message, reference, traceID string) {
 	e.Log(LogLevelFatal, message, reference, traceID, time.Now())
 }
 
