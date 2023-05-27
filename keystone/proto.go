b91c623fab1717b4ee1df76962643b12d0efbf0c
Brooke Bryan <brooke@bajb.net>
2023-05-27 09:36:36 +0100

    Entity from proto

---
+++ keystone/proto.go
@@ -0,0 +1,28 @@
+package keystone
+
+import (
+	"github.com/kubex/definitions-go/k4"
+	"github.com/kubex/keystone-go/proto"
+)
+
+func EntityFromProto(p *proto.EntityResponse) *Entity {
+	e := &Entity{}
+	e.ID = k4.IDFromString(p.EntityId)
+	e.Schema = GetScopedKey(p.Schema)
+	e.Properties = make(map[string]Property)
+	for _, prop := range p.Properties {
+		t := prop.GetTime().AsTime()
+		e.Properties[prop.Name] = Property{
+			Name:           prop.GetName(),
+			Type:           GetPropertyType(prop.GetType()),
+			Classification: GetClassification(prop.GetClassification()),
+			Text:           prop.GetText(),
+			Secret:         prop.GetSecureText(),
+			Int:            prop.GetInt(),
+			Bool:           prop.GetBool(),
+			Float:          float64(prop.GetFloat()),
+			Time:           &t,
+		}
+	}
+	return e
+}
