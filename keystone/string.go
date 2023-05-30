16c96d56820dbd36582c33652f5734bf6f9cb18e
Brooke Bryan <brooke@bajb.net>
2023-05-30 11:45:03 +0100

    String method on SecretString

--- keystone/string.go
+++ keystone/string.go
@@ -4,3 +4,10 @@ type SecretString struct {
 	Masked   string `json:"masked"`
 	Original string `json:"original"`
 }
+
+func (e SecretString) String() string {
+	if e.Original != "" {
+		return e.Original
+	}
+	return e.Masked
+}
